b6f6e49aeb99b97b029bb8a0a94bf8d4f62c5fbc
Christian Jaeger <ch@christianjaeger.ch>
2014-07-20 05:11:46 +0100

    use 100 stripes(?)
      buckets, ms real time (ms cpu time)
      1000      64 (104)
      10000     90 (128)
      100000    99 (168)
      1000000  154 (296)
      10000000 359 (704)

--- binning.scm
+++ binning.scm
@@ -154,7 +154,7 @@ unsigned int* res= ___BODY(___ARG5);
 int i;
 #pragma omp parallel for                                        \\
     shared(nvals,vals,nbuckets,buckets,res) private(i)          \\
-    schedule(dynamic,2000)
+    schedule(dynamic,100)
 for (i=0; i<nvals; i++) {
     double val= vals[i];
     int bi;
