be2cb540c59d269a2323a1079af5f8a0ce3c9361
Christian Jaeger <ch@christianjaeger.ch>
2014-07-20 04:37:32 +0100

    move lib code to lib.scm

--- binning.scm
+++ binning.scm
@@ -5,33 +5,7 @@
 
 (declare (standard-bindings) (extended-bindings))
 
-;; XX lib
-
-(def (integer-average a b)
-     (arithmetic-shift (+ a b) -1))
-
-(TEST
- > (integer-average 1 1)
- 1
- > (integer-average 1 2)
- 1
- > (integer-average 1 3)
- 2
- > (integer-average 0 3)
- 1
- > (integer-average 3 0)
- 1
- > (integer-average 3 -4)
- -1)
-
-
-(def. f64vector.length f64vector-length)
-(def. f64vector.list f64vector->list)
-(def. list.f64vector list->f64vector)
-(def inexact exact->inexact)
-
-;; /lib
-
+(include "lib.scm")
 
 (def rs (make-random-source))
 (random-source-randomize! rs)
---
+++ lib.scm
@@ -0,0 +1,24 @@
+
+(def (integer-average a b)
+     (arithmetic-shift (+ a b) -1))
+
+(TEST
+ > (integer-average 1 1)
+ 1
+ > (integer-average 1 2)
+ 1
+ > (integer-average 1 3)
+ 2
+ > (integer-average 0 3)
+ 1
+ > (integer-average 3 0)
+ 1
+ > (integer-average 3 -4)
+ -1)
+
+
+(def. f64vector.length f64vector-length)
+(def. f64vector.list f64vector->list)
+(def. list.f64vector list->f64vector)
+(def inexact exact->inexact)
+
